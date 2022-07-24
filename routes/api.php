8897cefe3f88d39f109a448fcec3aa8e2ab6e828
Esbol Moldrahmetov <esbolarai@gmail.com>
2022-07-24 09:38:47 +0600

    template commit

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::resource('vacancy', VacancyController::class);
 Route::resource('response', ResponseController::class);
+Route::post('/user-liked-vacancies/like', [ResponseController::class, 'like']);
